bdcbc04a5bad98537d099d5b1affa44b73cf57c8
Kevin Liou <kwliou@hotmail.com>
2010-10-30 09:06:32 -0700

    some formatting

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+  def index
+    @current_user = current_user
+    @courses = Course.all
+    @user_session = UserSession.new
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @courses }
+    end
+  end
+end
+
