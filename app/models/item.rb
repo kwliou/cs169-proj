4ae7173a7cbda76f994cfe880779ec0003b62702
Derek Nelson <derekjn@gmail.com>
2010-11-01 21:49:52 -0700

    Schema change

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :course
+  has_many :blurbs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,15 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101102034918) do
-
-  create_table "assignments", :force => true do |t|
-    t.string   "name"
-    t.decimal  "points_possible"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20101102044510) do
 
   create_table "blurbs", :force => true do |t|
     t.string   "text"
@@ -55,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20101102034918) do
     t.float    "weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "posts", :force => true do |t|
