c0e0994d7053cbb8a7408da9d87a8dba6eb84f14
Kevin Liou <kwliou@hotmail.com>
2010-11-02 06:07:16 -0700

    finished post controller specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,9 +12,8 @@ describe PostsController do
 
   describe "GET index" do
     it "assigns current user's posts as @posts" do
-      #Post.stub(:find).with(:all).and_return([mock_post])
-      User.stub(:find).and_return(mock_user)
-      mock_user.stub(:posts).and_return([mock_post])
+      User.should_receive(:find).and_return(mock_user)
+      mock_user.stub(:posts).and_return([mock_post]) #Post.stub(:find).with(:all).and_return([mock_post])
       get :index, :user_id => 1
       assigns[:posts].should == [mock_post]
     end
@@ -22,7 +21,7 @@ describe PostsController do
 
   describe "GET show" do
     it "assigns the requested post as @post" do
-      User.stub(:find).and_return(mock_user)
+      User.should_receive(:find).and_return(mock_user)
       mock_user.stub(:posts).and_return(Post)
       Post.stub(:find).with("37").and_return(mock_post)
       get :show, :user_id => 1, :id => "37"
@@ -32,8 +31,8 @@ describe PostsController do
 
   describe "GET new" do
     it "assigns a new post as @post" do
-      User.stub(:find).and_return(mock_user)
-      mock_user.should_receive(:posts).and_return(Post)
+      User.should_receive(:find).and_return(mock_user)
+      mock_user.stub(:posts).and_return(Post)
       Post.stub(:build).and_return(mock_post)
       get :new, :user_id => 1
       assigns[:post].should equal(mock_post)
@@ -42,10 +41,10 @@ describe PostsController do
 
   describe "GET edit" do
     it "assigns the requested post as @post" do
-      User.stub(:find).and_return(mock_user)
-      mock_user.should_receive(:posts).and_return(Post)
+      User.should_receive(:find).and_return(mock_user)
+      mock_user.stub(:posts).and_return(Post)
       Post.stub(:find).with("37").and_return(mock_post)
-      get :edit, :user_id => 1, :id => "37"
+      get :edit, :id => "37"
       assigns[:post].should equal(mock_post)
     end
   end
@@ -54,16 +53,16 @@ describe PostsController do
 
     describe "with valid params" do
       it "assigns a newly created post as @post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:build).with({'these' => 'params'}).and_return(mock_post(:save => true))
         post :create, :user_id => 1, :post => {:these => 'params'}
         assigns[:post].should equal(mock_post)
       end
 
       it "redirects to the created post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:build).and_return(mock_post(:save => true))
         post :create, :user_id => 1, :post => {}
         response.should redirect_to(user_post_url(mock_user, mock_post))
@@ -72,16 +71,16 @@ describe PostsController do
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved post as @post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:build).with({'these' => 'params'}).and_return(mock_post(:save => false))
         post :create, :user_id => 1, :post => {:these => 'params'}
         assigns[:post].should equal(mock_post)
       end
 
       it "re-renders the 'new' template" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:build).and_return(mock_post(:save => false))
         post :create, :user_id => 1, :post => {}
         response.should render_template('new')
@@ -94,24 +93,24 @@ describe PostsController do
 
     describe "with valid params" do
       it "updates the requested post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.should_receive(:find).with("37").and_return(mock_post)
         mock_post.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :user_id => 1, :id => "37", :post => {:these => 'params'}
       end
 
       it "assigns the requested post as @post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:find).and_return(mock_post(:update_attributes => true))
         put :update, :user_id => 1, :id => "1"
         assigns[:post].should equal(mock_post)
       end
 
       it "redirects to the post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:find).and_return(mock_post(:update_attributes => true))
         put :update, :user_id => 1, :id => "1"
         response.should redirect_to(user_post_url(mock_user, mock_post))
@@ -120,24 +119,24 @@ describe PostsController do
 
     describe "with invalid params" do
       it "updates the requested post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.should_receive(:find).with("37").and_return(mock_post)
         mock_post.should_receive(:update_attributes).with({'these' => 'params'})
         put :update, :user_id => 1, :id => "37", :post => {:these => 'params'}
       end
 
       it "assigns the post as @post" do
-        User.stub(:find).and_return(mock_user)
-        mock_user.should_receive(:posts).and_return(Post)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:find).and_return(mock_post(:update_attributes => false))
         put :update, :user_id => 1, :id => "1"
         assigns[:post].should equal(mock_post)
       end
 
       it "re-renders the 'edit' template" do
-        mock_user.should_receive(:posts).and_return(Post)
-        User.stub(:find).and_return(mock_user)
+        User.should_receive(:find).and_return(mock_user)
+        mock_user.stub(:posts).and_return(Post)
         Post.stub(:find).and_return(mock_post(:update_attributes => false))
         put :update, :user_id => 1, :id => "1"
         response.should render_template('edit')
@@ -148,16 +147,16 @@ describe PostsController do
 
   describe "DELETE destroy" do
     it "destroys the requested post" do
-      User.stub(:find).and_return(mock_user)
-      mock_user.should_receive(:posts).and_return(Post)
+      User.should_receive(:find).and_return(mock_user)
+      mock_user.stub(:posts).and_return(Post)
       Post.should_receive(:find).with("37").and_return(mock_post)
       mock_post.should_receive(:destroy)
       delete :destroy, :user_id => 1, :id => "37"
     end
 
     it "redirects to the posts list" do
-      User.stub(:find).and_return(mock_user)
-      mock_user.should_receive(:posts).and_return(Post)
+      User.should_receive(:find).and_return(mock_user)
+      mock_user.stub(:posts).and_return(Post)
       Post.stub(:find).and_return(mock_post(:destroy => true))
       delete :destroy, :user_id => 1, :id => "1"
       response.should redirect_to(user_posts_url(mock_user))
