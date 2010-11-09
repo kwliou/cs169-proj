41901e4aba2592ac05c6cc6478022e5bb9260891
Kevin Liou <kwliou@hotmail.com>
2010-11-08 16:56:18 -0800

    edited main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
     @user_session = UserSession.new
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @courses }
+      # format.xml  { render :xml => @courses }
     end
   end
   def mobile
@@ -14,7 +14,7 @@ class MainController < ApplicationController
     @user_session = UserSession.new
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @courses }
+      # format.xml  { render :xml => @courses }
     end
   end
 end
