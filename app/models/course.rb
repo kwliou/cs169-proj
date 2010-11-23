f425001565416f8ed19d3cf307286680446458bd
Derek Nelson <derekjn@gmail.com>
2010-11-22 21:54:47 -0800

    Course page fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,7 +19,7 @@ class Course < ActiveRecord::Base
   }
   
   def Course.unabbr(abbr)
-    @@abbr.index(abbr.downcase) || abbr
+    @@abbr.index(abbr.downcase) || abbr.downcase.titleizev2
   end
   def dept
     (@@abbr[department] || department).upcase
