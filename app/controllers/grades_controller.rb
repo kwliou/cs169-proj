86cd8008ef3233ccedc2319d8d2efffb1faefc4c
Kevin Liou <kwliou@hotmail.com>
2010-11-23 06:44:53 -0800

    updated nav

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -4,7 +4,7 @@ class GradesController < ApplicationController
   # GET /grades
   # GET /grades.xml
   def index
-    @course = Course.find(params[:id])
+    @course = Course.find_by_param(params[:id])
     @current_user = current_user
     @grades = @current_user.grades
     
