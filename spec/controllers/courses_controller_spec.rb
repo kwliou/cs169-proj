7b4dfeb700f5367417f21d2df9004c3763cf5889
Steven Green <steven@shadowfax.(none)>
2010-11-23 07:05:16 -0800

    partially fixed course_controller methods. Need some help on create.

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe CoursesController do
 
+  before :each do 
+    @current_user = mock_model(User) 
+    controller.stub!(:current_user).and_return(@current_user) 
+  end 
+  
   def mock_course(stubs={})
     @mock_course ||= mock_model(Course, stubs)
   end
@@ -16,8 +21,8 @@ describe CoursesController do
 
   describe "GET show" do
     it "assigns the requested course as @course" do
-      Course.stub(:find).with("37").and_return(mock_course)
-      get :show, :id => "37"
+      Course.stub(:find_by_param).with("COMPSCI_169").and_return(mock_course)
+      get :show, :id => "COMPSCI_169"
       assigns[:course].should equal(mock_course)
     end
   end
@@ -32,8 +37,8 @@ describe CoursesController do
 
   describe "GET edit" do
     it "assigns the requested course as @course" do
-      Course.stub(:find).with("37").and_return(mock_course)
-      get :edit, :id => "37"
+      Course.stub(:find_by_param).with("COMPSCI_169").and_return(mock_course)
+      get :edit, :id => "COMPSCI_169"
       assigns[:course].should equal(mock_course)
     end
   end
@@ -74,40 +79,40 @@ describe CoursesController do
 
     describe "with valid params" do
       it "updates the requested course" do
-        Course.should_receive(:find).with("37").and_return(mock_course)
+        Course.should_receive(:find_by_param).with("COMPSCI_169").and_return(mock_course)
         mock_course.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :course => {:these => 'params'}
+        put :update, :id => "COMPSCI_169", :course => {:these => 'params'}
       end
 
       it "assigns the requested course as @course" do
-        Course.stub(:find).and_return(mock_course(:update_attributes => true))
-        put :update, :id => "1"
+        Course.stub(:find_by_param).and_return(mock_course(:update_attributes => true))
+        put :update, :id => "COMPSCI_169"
         assigns[:course].should equal(mock_course)
       end
 
       it "redirects to the course" do
-        Course.stub(:find).and_return(mock_course(:update_attributes => true))
-        put :update, :id => "1"
+        Course.stub(:find_by_param).and_return(mock_course(:update_attributes => true))
+        put :update, :id => "COMPSCI_169"
         response.should redirect_to(course_url(mock_course))
       end
     end
 
     describe "with invalid params" do
       it "updates the requested course" do
-        Course.should_receive(:find).with("37").and_return(mock_course)
+        Course.should_receive(:find_by_param).with("COMPSCI_169").and_return(mock_course)
         mock_course.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :course => {:these => 'params'}
+        put :update, :id => "COMPSCI_169", :course => {:these => 'params'}
       end
 
       it "assigns the course as @course" do
-        Course.stub(:find).and_return(mock_course(:update_attributes => false))
-        put :update, :id => "1"
+        Course.stub(:find_by_param).and_return(mock_course(:update_attributes => false))
+        put :update, :id => "COMPSCI_169"
         assigns[:course].should equal(mock_course)
       end
 
       it "re-renders the 'edit' template" do
-        Course.stub(:find).and_return(mock_course(:update_attributes => false))
-        put :update, :id => "1"
+        Course.stub(:find_by_param).and_return(mock_course(:update_attributes => false))
+        put :update, :id => "COMPSCI_169"
         response.should render_template('edit')
       end
     end
@@ -116,14 +121,14 @@ describe CoursesController do
 
   describe "DELETE destroy" do
     it "destroys the requested course" do
-      Course.should_receive(:find).with("37").and_return(mock_course)
+      Course.should_receive(:find_by_param).with("COMPSCI_169").and_return(mock_course)
       mock_course.should_receive(:destroy)
-      delete :destroy, :id => "37"
+      delete :destroy, :id => "COMPSCI_169"
     end
 
     it "redirects to the courses list" do
-      Course.stub(:find).and_return(mock_course(:destroy => true))
-      delete :destroy, :id => "1"
+      Course.stub(:find_by_param).and_return(mock_course(:destroy => true))
+      delete :destroy, :id => "COMPSCI_169"
       response.should redirect_to(courses_url)
     end
   end
