9055f2a6d5ac66653ee8cd28407f9e6a18269008
Kevin Liou <kwliou@hotmail.com>
2010-12-01 11:52:34 -0800

    started items page

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,25 @@ class Post < ActiveRecord::Base
     time = created_at.getlocal.strftime("%I:%M %p")
     time.first == '0' ? time[1..-1] : time
   end
+  def time_ago
+    time = -(updated_at.getlocal - DateTime.current)
+    day_sec = 24*60*60
+    hour_sec = 60*60
+    min_sec = 60
+    if time > day_sec
+      str = "#{(time/day_sec).to_i} days"
+      #time %= day_sec
+    elsif time > hour_sec
+      str = "#{(time/hour_sec).to_i} hrs"
+      #time %= hour_sec
+    elsif time > min_sec
+      str = "#{(time/min_sec).to_i} min"
+      #time %= min_sec
+    else
+      str = "#{(time.to_i).to_i} sec"
+    end
+    str += " ago"
+  end
   def sanitize # for views only
     ActionController::Base.helpers.sanitize(body.gsub("\n", "<br />"), :attributes => 'abbr alt cite class datetime height href name src title width rowspan colspan rel')
   end
