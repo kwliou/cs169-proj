9e0f1ed49e27d10bb61dc47fa680fb8611683e62
Derek Nelson <derekjn@gmail.com>
2010-12-13 10:50:27 -0800

    Changed migration

--- db/migrate/20101212201900_remove_short_and_long_names.rb
+++ db/migrate/20101212201900_remove_short_and_long_names.rb
@@ -1,13 +1,11 @@
 class RemoveShortAndLongNames < ActiveRecord::Migration
   def self.up
     remove_column :courses, :short_title
-    remove_column :courses, :long_title
     remove_column :courses, :title
   end
 
   def self.down
     add_column :courses, :short_title, :string
-    add_column :courses, :long_title, :string
     add_column :courses, :title, :string
   end
 end
