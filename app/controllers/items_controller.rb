8243f39d08c0bb984893916ebf646712962b05c0
Kevin Liou <kwliou@hotmail.com>
2010-12-14 11:37:57 -0800

    updated main page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -109,7 +109,7 @@ private
     redirect_to root_url if @current_user.nil?
   end
   def get_course
-    @course = Course.find_by_param(params[:course_id]) if params[:course_id]
+    @course = Course.find_by_param(params[:course_id])
   end
   def get_item
     @item = @course.items.find(:first, :conditions => ['lower(name) = ?', params[:id].downcase.gsub('_', ' ')]) if params[:id]
